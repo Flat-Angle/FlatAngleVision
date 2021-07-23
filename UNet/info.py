d1e354762b958767408f06ac0660699e4d713c1e
DaY1zz <66411125+DaY1zz@users.noreply.github.com>
2021-07-23 14:36:10 +0800

    Add files via upload

---
+++ UNet/info.py
@@ -0,0 +1,10 @@
+MODULE_SIZE = 128
+OUTPUT_CHANNELS = 3
+SRC_PATH = 'D:/PyCharm 2021.1.3/PycharmProjects/flatangleAPI/Segmentation/UNet/src'
+IMG_PATH = SRC_PATH + '/img'
+MASK_PATH = SRC_PATH + '/mask'
+DATASET_PATH = SRC_PATH + '/dataset'
+MODULE_PATH = SRC_PATH + '/model'
+UN_PRODUCED_FILE_PTAH = r'D:\PyCharm 2021.1.3\PycharmProjects\flatangleAPI\before'
+PRODUCED_FILE_PATH = SRC_PATH + '/../aft'
+EPOCHS_NUM = 10
